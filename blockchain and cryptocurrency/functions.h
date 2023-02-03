0022ebdb05bf6508f25d0f4abb6d4ea86d0d6eea
Immortality-IMT <94738562+Immortality-IMT@users.noreply.github.com>
2023-02-03 12:51:59 +0700

    Add files via upload
    Added kecaak and base58, the private key and public key are now stored in hex and the address in base58, so the user does not send the private key by mistake. Public and private are now generated using  Elliptic Curve Cryptography (ECC), specifically secp256k1 curve.

--- blockchain and cryptocurrency/functions.h
+++ blockchain and cryptocurrency/functions.h
@@ -1,18 +1,25 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <openssl/rand.h>
-#include <openssl/ec.h>
-#include <openssl/objects.h>
-#include <openssl/evp.h>
-#include <openssl/buffer.h>
-#include <openssl/ecdsa.h>
-#include <openssl/sha.h>
+#include <limits.h>
+#include <memory.h>
 #include <openssl/bio.h>
+#include <openssl/bn.h>
+#include <openssl/buffer.h>
+#include <openssl/crypto.h>
+#include <openssl/ec.h>
+#include <openssl/ecdsa.h>
+#include <openssl/evp.h>
+#include <openssl/objects.h>
+#include <openssl/rand.h>
+#include <openssl/ripemd.h>
+#include <openssl/sha.h>
 #include <openssl/ssl.h>
 #include <sqlite3.h>
-#include <openssl/sha.h>
-#include <openssl/bio.h>
 #include <time.h>
 #include <zlib.h>
 
@@ -25,11 +32,11 @@
 #define DB_BLOCKCHAIN "DB/blockchain.db"
 
 struct wallet {
-    char private_key_b64[45]; //secret key used to ensure your identity
-    char public_key_b64[65]; //use address instead
-    char address[45]; //what you send to get paid or to pay
+    char private_key[66]; //secret hex encoded key used to secure funds
+    char public_key[66]; //hdex encoded, use address instead
+    char address[36]; //what you send to get pay or pay
     char statement[256]; //bank statement of past transactions
-    char balance[65]; //how much money you have
+    char balance[32]; //how much money in the account
 };
 
 // Definition of a transaction that becomes part of a block
@@ -72,13 +79,15 @@ struct coinbase_transaction {
 //wallet.c
 BIGNUM* private_key();
 unsigned char* public_key(BIGNUM* private_key);
-char* public_key_to_hex(unsigned char* public_key);
-char* public_key_to_address(unsigned char* public_key);
-void get_keys(BIGNUM* private_key_ptr,unsigned char* public_key_ptr, char* private_key_b64, char* public_key_b64, char* address);
+void generate_new_address(const unsigned char *public_key, unsigned char *address);
+void private_key_to_hex(BIGNUM *private_key_ptr, char hex_private_key[66]);
+void public_key_to_hex(unsigned char* public_key, char hex_public_key[66]);
 int generate_address();
 void print_table(const char* db_name, const char* table_name);
 static int callback(void *data, int argc, char **argv, char **col_name);
 int main();
+
+char* public_key_to_address(unsigned char* public_key, int public_key_len);
 
 //transactions.c
 int create_transaction();
@@ -102,4 +111,24 @@ int start_blockchain();
 int verify_transaction();
 int verify_block();
 int verify_chain();
+
+//base58.c
+int base58_encode(const char *in, size_t in_len, char *out, size_t *out_len);
+int base58_decode(const char *in, size_t in_len, char *out, size_t *out_len);
+
+//keccak.c
+#ifndef KECCAK_FIPS202_H
+    #define KECCAK_FIPS202_H
+    #define __STDC_WANT_LIB_EXT1__ 1
+    #define decshake(bits) \
+      int shake##bits(uint8_t*, size_t, const uint8_t*, size_t);
+    #define decsha3(bits) \
+      int sha3_##bits(uint8_t*, size_t, const uint8_t*, size_t);
+    decshake(128)
+    decshake(256)
+    decsha3(224)
+    decsha3(256)
+    decsha3(384)
+    decsha3(512)
+#endif
 
